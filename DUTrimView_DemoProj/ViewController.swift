7e4495682c12862acda94854adf8390e046f076d
mohamed saeed <mohedsh@gmail.com>
2022-09-16 13:47:16 +0200

    Initial Commit

---
+++ DUTrimView_DemoProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DUTrimView_DemoProj
+//
+//  Created by mohamed saeed on 16/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
